0df9fd7819ce569230e37448f35ef17effc9678c
lee byeonghoon <qudgns200@gmail.com>
2019-12-21 03:09:27 +0900

    Study JS

--- clock.js
+++ clock.js
@@ -6,11 +6,13 @@ function getTime() {
     const minute = date.getMinutes();
     const hours = date.getHours();
     const second = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minute}:${second}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minute < 10 ? `0${minute}` : minute}:${second < 10 ? `0${second}`: second}
+                           `;
 }
 
 function init() {
     getTime();
+    setInterval(getTime, 1000);
 }
 
 init();
---
+++ greetings.js
@@ -0,0 +1,42 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greetings = document.querySelector(".js-greetings");
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing"
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreetings(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreetings(text) {
+    form.classList.remove(SHOWING_CN);
+    greetings.classList.add(SHOWING_CN);
+    greetings.innerHTML = `Hello ${text}`
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null) {
+        askForName();
+    } else {
+        paintGreetings(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init();
